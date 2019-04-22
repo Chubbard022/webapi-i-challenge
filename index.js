1d1427782b19d1daae24d8658108299d7fb7c1bc
Chubbard022 <chubbard022@gmail.com>
2019-04-22 12:23:45 -0700

    first get method working, returning json list of users

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // implement your API here
 const express = require("express");
+const db = require("./data/db")
 
 const server = express();
+server.use(express.json())
 
 server.get('/', (req, res) => {
     res.send("IT WORKS");
   });
-  
+
+//server get method will return a list in JSON format
+server.get("/api/users",(req,res)=>{
+    db.find()
+    .then(users=>{
+        res.json(users)
+    })
+    .catch(err=>{
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+})
 
 
 server.listen(5000, ()=>{
